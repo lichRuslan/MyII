c8591ed2d79bc38479105bd2045efc3bf176cd97
Ruslan Kafanov <rkafanov@askartec.com>
2018-10-02 14:05:11 +0500

    add app

---
+++ app.js
@@ -0,0 +1,27 @@
+class user{
+
+    name(name) {
+        this.name = name;
+      }
+    // var userInfo = {
+    //     que1 : true,
+    //     que2 : true,
+    //     que3 : true,
+    //     que4 : true,
+    //     que5 : true,
+    //     que6 : true,
+    //     que7 : true,
+    //     que8 : true,
+    //     que9 : true,
+    //     que10 : true,
+    //     que11 : true,
+    //     que16 : true  
+    };
+    return userInfo;
+}
+
+
+function questions(user){
+    console.log(user);
+}
+question(user);
